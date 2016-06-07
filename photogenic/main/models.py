cd0ae2d3e5035ec014be164b5e01ab2c5426f53b
Penina Wanjiru <penina.wanjiru@andela.com>
2016-06-07 18:32:48 +0300

    [Feature] created image model

---
+++ photogenic/main/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Image(models.Model):
+
+    user = models.ForeignKey(User, related_name="user")
+    image = models.ImageField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
