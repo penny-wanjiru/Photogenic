33008611859146455b8b15c72ef75c897968c2b3
Penina Wanjiru <penina.wanjiru@andela.com>
2016-07-05 01:15:28 +0300

    [chore] change static reference

--- photogenic/settings/base.py
+++ photogenic/settings/base.py
@@ -126,10 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static'),
 )
 STATIC_URL = '/static/'
 AUTHENTICATION_BACKENDS = (
