5c5439448811eec1bbb01d38f35fb1cf2992e700
Penina Wanjiru <penina.wanjiru@andela.com>
2016-07-07 19:40:19 +0300

    [Chore] changed travis file

---
+++ photogenic/settings/test.py
@@ -0,0 +1,10 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_photoapp',
+        'USER': 'postgres',
+        'PASSWORD': 'Andela2015'
+    }
+}
